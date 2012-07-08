07c95ca81f033382768cf631d41843c3bd030823
Ruben Undheim <ruben.undheim@gmail.com>
2012-07-08 23:54:09 +0200

    Introduced cmake.
    It is now compilng, but some configuration is still missing.

---
+++ src/config.h
@@ -0,0 +1,17 @@
+/* Define to 1 if you have <alsa/asoundlib.h> header file */
+#define HAVE_ALSA 1
+
+/* Define to 1 if you have <curl/curl.h> header file */
+/* #undef HAVE_CURL */
+
+/* Define to 1 if you have GTK 2.0 installed */
+/* #undef HAVE_GTK */
+
+/* Define to 1 if you have <mysql/mysql.h> header file */
+/* #undef HAVE_MYSQL */
+
+/* Name of package */
+#define PACKAGE "gnuais"
+
+/* Version number of package */
+#define VERSION "0.2.0"
