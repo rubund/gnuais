452183eea02b7f988902e0ea458c45f796344cb1
Ruben Undheim <ruben.undheim@gmail.com>
2012-07-10 11:20:52 +0200

    Added control data to deb package

--- createdeb.sh
+++ createdeb.sh
@@ -5,8 +5,8 @@ VERSION=0.2.4
 rm -rf createdeb/
 mkdir createdeb
 mkdir createdeb/$PACKAGENAME-$VERSION
-git archive master | tar -x -C createdeb/$PACKAGENAME-$VERSION
+git archive ubuntu | tar -x -C createdeb/$PACKAGENAME-$VERSION
 cd createdeb/$PACKAGENAME-$VERSION
 cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr .
 dh_make --createorig
-debuild
+debuild -i -us -uc -b
