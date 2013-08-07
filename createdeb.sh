a5737da9a94e0614572d2c68a6e8d1c288629692
Ruben Undheim <ruben.undheim@gmail.com>
2013-08-07 22:53:38 +0200

    Updated script for creating deb package

--- createdeb.sh
+++ createdeb.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 PACKAGENAME=gnuais
-VERSION=0.2.7alpha
+VERSION=0.2.8
 rm -rf createdeb/
 mkdir createdeb
 mkdir createdeb/$PACKAGENAME-$VERSION
-git archive ubuntu | tar -x -C createdeb/$PACKAGENAME-$VERSION
+git archive master | tar -x -C createdeb/$PACKAGENAME-$VERSION
+cp -r -v debian createdeb/$PACKAGENAME-$VERSION/
 cd createdeb/$PACKAGENAME-$VERSION
-cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr .
 dh_make --createorig -s
-debuild -i -us -uc -b
+dpkg-buildpackage -us -uc
+#cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr .
+#debuild -i -us -uc -b
