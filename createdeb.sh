525fcafa09875051ef723ba5e290992618e62445
Ruben Undheim <ruben.undheim@gmail.com>
2014-05-16 22:10:45 +0200

    simplifying build of several versions with the same orig-source

--- createdeb.sh
+++ createdeb.sh
@@ -4,9 +4,11 @@ PACKAGENAME=gnuais
 VERSION=0.3.1
 rm -rf createdeb/
 mkdir createdeb
+#rm -rf createdeb/$PACKAGENAME-$VERSION
 mkdir createdeb/$PACKAGENAME-$VERSION
 git archive master | tar -x -C createdeb/$PACKAGENAME-$VERSION
 cd createdeb
+#tar -xvf $PACKAGENAME\_$VERSION.orig.tar.gz
 tar -czvf $PACKAGENAME\_$VERSION.orig.tar.gz $PACKAGENAME-$VERSION
 cp -r -v ../debian $PACKAGENAME-$VERSION/
 cd $PACKAGENAME-$VERSION
