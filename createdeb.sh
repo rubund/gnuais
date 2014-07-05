17a17bc8c786aeb2b38339d04ba2c5686aa92b2b
Ruben Undheim <ruben.undheim@gmail.com>
2014-07-05 13:22:41 +0200

    updated createdeb.sh

--- createdeb.sh
+++ createdeb.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 PACKAGENAME=gnuais
-VERSION=0.3.1
-#rm -rf createdeb/
-#mkdir createdeb
-rm -rf createdeb/$PACKAGENAME-$VERSION
+VERSION=0.3.2
+rm -rf createdeb/
+mkdir createdeb
+#rm -rf createdeb/$PACKAGENAME-$VERSION
 mkdir createdeb/$PACKAGENAME-$VERSION
-#git archive master | tar -x -C createdeb/$PACKAGENAME-$VERSION
+git archive master | tar -x -C createdeb/$PACKAGENAME-$VERSION
 cd createdeb
-tar -xvf $PACKAGENAME\_$VERSION.orig.tar.gz
-#tar -czvf $PACKAGENAME\_$VERSION.orig.tar.gz $PACKAGENAME-$VERSION
+#tar -xvf $PACKAGENAME\_$VERSION.orig.tar.gz
+tar -czvf $PACKAGENAME\_$VERSION.orig.tar.gz $PACKAGENAME-$VERSION
 cp -r -v ../debian $PACKAGENAME-$VERSION/
 cd $PACKAGENAME-$VERSION
 dpkg-buildpackage -S
