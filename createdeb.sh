7b6a637d0faccedc5349230c3efb4e9e5689ccbc
Ruben Undheim <ruben.undheim@gmail.com>
2014-09-04 17:01:35 +0000

    Using correct version

--- createdeb.sh
+++ createdeb.sh
@@ -6,7 +6,7 @@ rm -rf createdeb/
 mkdir createdeb
 #rm -rf createdeb/$PACKAGENAME-$VERSION
 mkdir createdeb/$PACKAGENAME-$VERSION
-git archive master | tar -x -C createdeb/$PACKAGENAME-$VERSION
+git archive $VERSION | tar -x -C createdeb/$PACKAGENAME-$VERSION
 cd createdeb
 #tar -xvf $PACKAGENAME\_$VERSION.orig.tar.gz
 tar -czvf $PACKAGENAME\_$VERSION.orig.tar.gz $PACKAGENAME-$VERSION
